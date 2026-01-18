3a469a07d455110baaab6c2e1e15a285cd06d901
fadlidzilikram2 <fadlidzilikram20@gmail.com>
2026-01-19 03:09:16 +0700

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,7 @@
 import joblib
 
 def load_models():
-    tfidf = joblib.load("tfidf.pkl")
-    lda = joblib.load("lda_model.pkl")
-    nb = joblib.load("nb_model.pkl")
-    return tfidf, lda, nb
+   
+    tfidf = joblib.load('tfidf.pkl')
+    nb = joblib.load('nb_model.pkl')
+    return tfidf, nb
